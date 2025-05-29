fb92e03633f2bd7f6494d1a1220dbdc94b489486
DSllide <dreatsllide@gmail.com>
2025-05-29 17:04:09 +0300

    Add files via upload

---
+++ C++Lab1Ex4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <locale>;
+using namespace std;
+
+int main()
+{
+
+    int a, b, c;
+    cout << "Input a: ";
+    cin >> a;
+    cout << "Input b: ";
+    cin >> b;
+    cout << "Input c: ";
+    cin >> c;
+    int S = 2 * (a * b + a * c + b * c);
+    int V = a * b * c;
+    cout << "S parallelepiped  = " << S << "\nV parallelepiped = " << V;
+}
+
