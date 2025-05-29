4bf81e5ec690b2bfc04f6b7d30bcf906e12d31c1
DSllide <dreatsllide@gmail.com>
2025-05-29 17:09:26 +0300

    Add files via upload

---
+++ C++Lab1Ex5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>  
+
+using namespace std;
+
+int main()
+{
+    cout << "Input R: ";
+    float R;
+    cin >> R;
+
+    float Pi = 3.14;
+    float circle_length = 2 * Pi * R;
+    float circle_S = Pi * R * R;
+
+    cout << fixed << setprecision(2);
+    cout << "Circle area = " << circle_S << "\nCircle length = " << circle_length << endl;
+
+    return 0;
+}
