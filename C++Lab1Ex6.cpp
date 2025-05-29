d8e165719c7e347bd8e3b965661fdfae4b71b9f3
DSllide <dreatsllide@gmail.com>
2025-05-29 17:11:56 +0300

    Add files via upload

---
+++ C++Lab1Ex6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float P, R, I;
+    int T;
+
+    cout << "Enter the amount P: ";
+    cin >> P;
+
+    cout << "Enter the loan period T: ";
+    cin >> T;
+
+    cout << "Enter the interest rate R: ";
+    cin >> R;
+
+    I = (P * T * R) / 100;
+
+    cout << "Simple interest (as float): " << fixed << setprecision(2) << I << endl;
+    cout << "Simple interest (as int): " << static_cast<int>(I);
+
+    return 0;
+}
