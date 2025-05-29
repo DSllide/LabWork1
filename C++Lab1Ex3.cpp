989c1c2310405245b059b547df5b629369b81601
DSllide <dreatsllide@gmail.com>
2025-05-29 17:01:01 +0300

    Add files via upload

---
+++ C++Lab1Ex3.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>;
+#include <locale>;
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter number a: ";
+    int a;
+    cin >> a;
+    cout << "Enter number b: ";
+    int b;
+    cin >> b;
+    int summary = a + b;
+    int difference = a - b;
+    int multiple = a * b;
+    cout << "Sum a i b = " << summary << "\nDifference a i b = " << difference << "\nProduct a i b = " << multiple;
+}
